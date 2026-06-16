c0b85d6f31ed0df0429f9c4c7c9926f4f5f143df
dtsud <d.tsudayuki@gmail.com>
2026-06-16 12:06:59 +0800

    feat: GroupType实体与Mapper支持限报字段读写

--- SportBackend/src/main/java/com/dlust/sportbackend/entity/GroupType.java
+++ SportBackend/src/main/java/com/dlust/sportbackend/entity/GroupType.java
@@ -8,6 +8,8 @@ public class GroupType {
     private Long id;
     private Long sportsMeetingId;
     private String name;
+    private Integer perPersonLimit;   // 每人限报项目数(0=不限)
+    private String limitEventIds;     // 限报选中项目ID数组的JSON字符串(如"[1,2,3]"),DB为JSON列
     private LocalDateTime createTime;
     private LocalDateTime updateTime;
 }
